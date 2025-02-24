42b34dec613ec2604936f5a07350cf892859d34b
tectiv3 <git@getpanda.me>
2025-02-24 17:52:14 +0000

    Fix php warning

--- src/SimpleSoftwareIO/QrCode/BaconQrCodeGenerator.php
+++ src/SimpleSoftwareIO/QrCode/BaconQrCodeGenerator.php
@@ -55,7 +55,7 @@ class BaconQrCodeGenerator implements QrCodeInterface
      * @param Writer|null            $writer
      * @param RendererInterface|null $format
      */
-    public function __construct(Writer $writer = null, RendererInterface $format = null)
+    public function __construct(?Writer $writer = null, ?RendererInterface $format = null)
     {
         $format = $format ?: new Svg();
         $this->writer = $writer ?: new Writer($format);
